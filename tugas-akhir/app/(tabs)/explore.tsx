60ab3871e9333d4e0fec682a2f936ab2437580d6
Alvin Widi Nugroho <alvin.widi@ui.ac.id>
2025-08-15 12:52:13 +0700

    coba lagi

---
+++ tugas-akhir/app/(tabs)/explore.tsx
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function explore() {
+  return (
+    <View>
+      <Text>Explore</Text>
+    </View>
+  )
+}
